740494adc72450fb347fd071d0b7cde85e319608
viceroypenguin <stuart@turner-isageek.com>
2017-07-12 12:56:18 -0700

    Hopefully add nuget support

--- RBush/AssemblyInfo.cs
+++ RBush/AssemblyInfo.cs
@@ -1,3 +1,7 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("RBush.Test")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
